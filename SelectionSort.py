74c749a5dd8b2e38a441a688c9ab1992c66614e8
SiMoM0 <53409996+SiMoM0@users.noreply.github.com>
2020-11-26 23:28:05 +0100

    Selection sort .py file added

---
+++ SelectionSort.py
@@ -0,0 +1,23 @@
+#Selection Sort function in pyhton using an array of 50 elements for test
+import random as rd
+
+#Selection Sort function
+def selectionSort(arr):
+    for i in range(len(arr)):
+        min_index = i;
+        for j in range(i+1, len(arr)):
+            if arr[j]<arr[min_index]:
+                min_index = j;
+        #swap the positions
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+lista = []
+for i in range(50):
+    #insert in the list random numbers between 1 and 1000
+    lista.append(rd.randint(1, 1000))   
+print("List of numbers: ", lista)
+
+#sort the list with selection sort
+lista = selectionSort(lista)
+print("Sorted list: ", lista)
